3b015eba8bb3229bb41498c28f98d097118a2311
Hubert Pyzik <hubert.pyzik@schibsted.pl>
2018-01-09 22:29:10 +0100

    encapsulate state filtering

--- src/component/VisibleTodoList.jsx
+++ src/component/VisibleTodoList.jsx
@@ -1,38 +1,23 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 
+import { getVisibleTodos } from '../reducer/todoApp';
 import TodoList from './TodoList';
 import toggleTodo from '../action/toggleTodo';
 
-const getVisibleTodos = (todos, filter) => {
-    switch (filter) {
-        case 'all':
-            return todos;
-        case 'completed':
-            return todos.filter(t => t.completed);
-        case 'active':
-            return todos.filter(t => !t.completed);
-    }
-};
 
 const mapStateToProps = (state, { match: { params: { filter } } }) => {
     return {
         todos: getVisibleTodos(
-            state.todos,
+            state,
             filter || 'all',
         ),
     };
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        onTodoClick: id => dispatch(toggleTodo(id)),
-    }
-};
-
 const VisibleTodoList = withRouter(connect(
     mapStateToProps,
-    mapDispatchToProps,
+    { onTodoClick: toggleTodo },
 )(TodoList));
 
 export default VisibleTodoList;
